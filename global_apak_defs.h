4c6861ef518a4c9fbfc87d5b0f5eefaa1d875985
breton75 <breton75@yandex.ru>
2022-07-05 16:24:29 +0300

    Update global_apak_defs.h, hmi.cpp, and 108 more files...

---
+++ global_apak_defs.h
@@ -0,0 +1,16 @@
+﻿#ifndef GLOBAL_LIB_DEFS
+#define GLOBAL_LIB_DEFS
+
+#include <QMap>
+
+#define TYPE_DATA "data"
+#define TYPE_STAT "stat"
+
+namespace apak {
+
+
+
+}
+
+#endif // GLOBAL_LIB_DEFS
+
---
+++ kson/src/kson_global.h
@@ -0,0 +1,12 @@
+﻿#ifndef KSON_GLOBAL_H
+#define KSON_GLOBAL_H
+
+#include <QtCore/qglobal.h>
+
+#if defined(KSON_LIBRARY)
+#  define KSON_EXPORT Q_DECL_EXPORT
+#else
+#  define KSON_EXPORT Q_DECL_IMPORT
+#endif
+
+#endif // KSON_GLOBAL_H
