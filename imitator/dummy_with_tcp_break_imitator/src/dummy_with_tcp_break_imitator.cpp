b3bfa000eb3e8e696488b62702202b02e62cc590
Aleksander1975 <alx1home@gmail.com>
2022-11-07 15:11:54 +0300

    Update dummy_with_tcp_break_imitator.cpp, dummy_with_tcp_break_imitator.h, and 3 more files... Имитатор-заглушка для отладки команды разрыва TCP-соединения. Команда разрыва соединения выдаётся имитатором-заглушкой после приёма каждого десятого сообщения.

---
+++ imitator/dummy_with_tcp_break_imitator/src/dummy_with_tcp_break_imitator.cpp
@@ -0,0 +1,167 @@
+﻿#include "dummy_with_tcp_break_imitator.h"
+
+apak::SvDummyImitator::SvDummyImitator():
+  modus::SvAbstractProtocol()
+{
+}
+
+apak::SvDummyImitator::~SvDummyImitator()
+{
+  deleteLater();
+}
+
+bool apak::SvDummyImitator::configure(modus::DeviceConfig *config, modus::IOBuffer *iobuffer)
+// Эта функция вызывается серевером "mdserver" для всех имитаторов устройств.
+// Её цель - инициализировать все структуры, необходимые нам для конкретного
+// имитатора (для "заглушки" единственная структура данных - это номер посылки "m_transferNumber").
+{
+    // Сбросим номер посылки:
+    m_transferNumber = 0;
+
+    // Сбросим счётчик принятых сообщений;
+    m_receivedMessageCounter = 0;
+
+    try
+    {
+
+    p_config = config;
+    p_io_buffer = iobuffer;
+
+    // Заполняем структуру m_params параметром протокола обмена "заглушки" с системой АПАК
+    // (периодом поступления данных в мс от "заглушки" в систему АПАК)
+    m_params = Dummy::ProtocolParams::fromJson(p_config->protocol.params);
+
+    return true;
+    }
+    catch (SvException& e) {
+
+    p_last_error = e.error;
+    return false;
+  } 
+}
+
+bool apak::SvDummyImitator::bindSignal(modus::SvSignal* signal, modus::SignalBinding binding)
+// Эта функция вызывается сервером "mdserver" для всех сигналов "заглушки".
+// Так как у "заглушки" нет сигналов, то эта функция вызыватья не будет.
+{
+  try {
+
+    bool r = modus::SvAbstractProtocol::bindSignal(signal, binding);
+
+
+    Dummy::SignalParams signal_params = Dummy::SignalParams::fromJson(binding.params);
+
+    if(r)
+    {
+
+      if(binding.mode == modus::Master)
+      {
+      }
+      else
+      {
+      }
+    } // if (r)
+
+    return r;
+  } // try
+
+  catch(SvException& e)
+  {
+    p_last_error = e.error;
+    return false;
+  }
+}
+
+void apak::SvDummyImitator::signalUpdated(modus::SvSignal* signal)
+{
+  Q_UNUSED(signal);
+}
+
+void apak::SvDummyImitator::signalChanged(modus::SvSignal* signal)
+{
+  Q_UNUSED(signal);
+}
+
+void apak::SvDummyImitator::start()
+// В этой функции мы осуществляем привязку вызова функции send() к наступлению таймаута таймера "m_timer".
+{
+  m_timer = new QTimer;
+  connect(m_timer, &QTimer::timeout, this, &SvDummyImitator::send);
+  m_timer->start(m_params.send_interval);
+
+  // Привязываем вызов функции "messageFrom_DUMMY", к сигналу "modus::IOBuffer::dataReaded". Этот сигнал
+  // испускается интерфейсной частью по приходу данных от интерефейса (в нашем случае - от имитатора DUMMY).
+  connect(p_io_buffer, &modus::IOBuffer::dataReaded, this, &SvDummyImitator::messageFrom_DUMMY);
+
+  p_is_active = bool(p_config) && bool(p_io_buffer);
+}
+
+void apak::SvDummyImitator::send()
+// В этой функции мы формируем и помещаем в массив байт "m_send_data" пакет для передачи от "заглушки"
+// в систему АПАК (в соответствии с протоколом обмена) и инициируем передачу
+// этого пакета от протокольной к интерфейcной части имитатора (для передачи по линии связи).
+{
+    //qDebug () << "Вызов send()";
+
+    if(p_is_active)
+    {
+        // 1. В переменной "m_send_data" формируем пакет данных от "заглушки":
+        // то есть посылку, длиной в 250 байт, каждый байт которой содержит
+        // значение номера посылки "m_transferNumber".
+        m_send_data.clear();
+        m_send_data.resize (250);
+        m_send_data.fill(m_transferNumber, 250);
+
+        // 2. Увеличиваем значение "m_transferNumber" на 1.
+        if (m_transferNumber == 255)
+            m_transferNumber = 0;
+        else
+            m_transferNumber++;
+
+
+       //qDebug() << "Пакет (в окончательном виде) от ЗАГЛУШКИ-протокола";
+       //qDebug() << "Размер: " << m_send_data.length();
+       //qDebug() << "Содержание: " << m_send_data.toHex();
+
+
+       // 3. Передаём данные от протокольной к интерфейcной части (для передачи по линии связи):
+       p_io_buffer->output->mutex.lock();
+       p_io_buffer->output->setData(m_send_data);
+
+       emit message(QString(m_send_data.toHex()), lldbg, sv::log::mtNew);
+
+       p_io_buffer->output->setReady(true);
+       emit p_io_buffer->readyWrite(p_io_buffer->output);
+
+       p_io_buffer->output->mutex.unlock();
+
+    } //if(p_is_active)
+}
+
+// Функция, которая вызвается по готовности данных в интерфейсной части:
+void apak::SvDummyImitator::messageFrom_DUMMY(modus::BUFF* buffer)
+{
+    // Команду на разрыв соединения мы будем подавать тогда, когда знаем, что соединение
+    // было установлено. Это мы узнаем по тому что мы поличили данные от интерфейса.
+    // Чтобы видеть процесс "отсоединенея - присоединения клиента к серверу", будем
+    // выдвать команду на разрыв соединения, только после 10 принятых сообщений.
+
+    // Инкрементируем счётчик принятых сообщений;
+    m_receivedMessageCounter++;
+
+    if (m_receivedMessageCounter == 10)
+    {
+        qDebug() << "DUMMY_WITH_TCP_BREAK_CONNECTION: Выдаю команду на разрыв TCP-соединения";
+        emit p_io_buffer->say("breakConnection");
+
+        //Сбрасываем счётчик принятых сообщений:
+        m_receivedMessageCounter = 0;
+    }
+}
+/** ********** EXPORT ************ **/
+modus::SvAbstractProtocol* create()
+{
+  modus::SvAbstractProtocol* protocol = new apak::SvDummyImitator();
+  return protocol;
+}
+
---
+++ imitator/dummy_with_tcp_break_imitator/src/protocol_params.h
@@ -0,0 +1,155 @@
+﻿#ifndef DUMMY_IMITATOR_PARAMS
+#define DUMMY_IMITATOR_PARAMS
+
+#include <QString>
+#include <QtCore>
+
+#include <QJsonDocument>
+#include <QJsonObject>
+
+#include "../../../../svlib/SvException/svexception.h"
+#include "../../../../Modus/global/global_defs.h"
+
+// Имя параметра "период поступления данных от "заглушки" к АПАК",
+// используемое при описании "заглушки" в файле описания
+// имитаторов:
+#define SEND_INTERVAL   "interval"
+
+// Значение периода поступления данных от "заглушки" в систему АПАК ПО УМОЛЧАНИЮ
+// (данное значение берётся в том случае,
+// если в конфигурационном файле имитаторов усторойств для "заглушки" не задано
+// другое значение):
+#define DUMMY_DEFAULT_SEND_INTERVAL 1000
+
+namespace Dummy {
+
+  struct ProtocolParams
+  // Структура, хранящая параметр протокола - период в мс поступления данных от "заглушки" в систему АПАК.
+  {
+    // Период поступления данных в мс от "заглушки" в систему АПАК:
+    quint16 send_interval = DUMMY_DEFAULT_SEND_INTERVAL;
+
+    static ProtocolParams fromJson(const QString& json_string) //throw (SvException)
+    // Конструктор:
+
+    // Вход: строка, являющаяся отрывком, взятым из конфигурационного файла имитаторов для
+    // сервера "mdserver", и содержащем информацию только о "заглушке".
+
+    // Выход: Структура ProtocolParams, заполненная параметром "заглушки" (периодом поступления
+    // данных от "заглушки").
+    {
+      QJsonParseError err;
+      QJsonDocument jd = QJsonDocument::fromJson(json_string.toUtf8(), &err);
+
+      if(err.error != QJsonParseError::NoError)
+        throw SvException(err.errorString());
+
+      try {
+
+        return fromJsonObject(jd.object());
+
+      }
+      catch(SvException& e) {
+        throw e;
+      }
+    }
+
+    static ProtocolParams fromJsonObject(const QJsonObject &object) //throw (SvException)
+    // Конструктор:
+
+    // Вход: объект QJsonObject, описывающий "заглушку".
+
+    // Выход: Структура ProtocolParams, заполненная параметром "заглушки" (периодом поступления
+    // данных от "заглушки").
+   {
+      ProtocolParams p;
+      QString P;
+
+      P = SEND_INTERVAL;
+      if(object.contains(P)) {
+
+        if(object.value(P).toInt(-1) < 0)
+          throw SvException(QString(IMPERMISSIBLE_VALUE)
+                            .arg(P)
+                            .arg(object.value(P).toVariant().toString())
+                            .arg("Интервал отправки данных должен быть задан целым числом в миллисекундах"));
+
+        p.send_interval = object.value(P).toInt(DUMMY_DEFAULT_SEND_INTERVAL);
+
+      }
+      else
+        p.send_interval = DUMMY_DEFAULT_SEND_INTERVAL;
+
+      return p;
+
+    }
+
+    QString toString(QJsonDocument::JsonFormat format = QJsonDocument::Indented) const
+    {
+      QJsonDocument jd;
+      jd.setObject(toJsonObject());
+
+      return QString(jd.toJson(format));
+    }
+
+    QJsonObject toJsonObject() const
+    {
+      QJsonObject j;
+
+      j.insert(SEND_INTERVAL,   QJsonValue(send_interval).toInt());
+
+      return j;
+
+    }
+  };
+
+  struct SignalParams
+  // Структура, хранящая параметры сигнала имитатора устройства. Для "заглушки" не используется.
+  {
+    static SignalParams fromJson(const QString& json_string) //throw (SvException)
+    {
+      QJsonParseError err;
+      QJsonDocument jd = QJsonDocument::fromJson(json_string.toUtf8(), &err);
+
+      if(err.error != QJsonParseError::NoError)
+        throw SvException(err.errorString());
+
+      try {
+
+        return fromJsonObject(jd.object());
+
+      }
+      catch(SvException& e) {
+        throw e;
+      }
+    }
+
+    static SignalParams fromJsonObject(const QJsonObject &object) //throw (SvException)
+    {
+      SignalParams p;
+      QString P;
+
+      return p;
+
+    }
+
+    QString toString(QJsonDocument::JsonFormat format = QJsonDocument::Indented) const
+    {
+      QJsonDocument jd;
+      jd.setObject(toJsonObject());
+
+      return QString(jd.toJson(format));
+    }
+
+    QJsonObject toJsonObject() const
+    {
+      QJsonObject j;
+
+      return j;
+    }
+  };
+
+}
+
+#endif // DUMMY_IMITATOR_PARAMS
+
